5e3c1211603a1adbcd90b18e13e986bea20f2dbc
asdflj <33802162+asdflj@users.noreply.github.com>
2017-12-08 13:04:51 +0800

    Update energy_firstrun.lua

--- energy_ctrl/lib/energy_firstrun.lua
+++ energy_ctrl/lib/energy_firstrun.lua
@@ -4,7 +4,7 @@ mfsu={}
 mfe={}
 cesu={}
 bat={}
-all_energy=0
+local all_energy=0
 --获取配置文件数据--
 local function file_exists(path)
 file = io.open(path, 'rb')
@@ -47,11 +47,11 @@ print('輸入數字錯誤！')
 else
 if line_name~=nil then
 
-_G.energy_value={all_energy*(value/100)}
+_G.energy_value=all_energy*(value/100)
 else
-_G.energy_value={value/1000000}
+_G.energy_value=value/1000000
 end
-file:write('切換='.._G.energy_value[1])
+file:write('切換='.._G.energy_value)
 return
 end
 end
@@ -62,11 +62,11 @@ do
   local line_name=string.find(line,"=")
   local energy_text=string.sub(line,line_name+1)
   local energy_text=string.sub(energy_text,0,-1)
-  _G.energy_value={energy_text}
+  _G.energy_value=energy_text
 end
 end
 file_exists('energy.cfg') 
-if  false  then --检测文件是否存在
+if  file ~= nil then --检测文件是否存在
 file = io.open("energy.cfg","r")  
 energy_r ()
 else
@@ -75,5 +75,5 @@ e=1
 file = io.open("energy.cfg","w")
 energy_w ()
 end
-_G.energy={all_energy}
+_G.total_energy=all_energy
 file:close()
