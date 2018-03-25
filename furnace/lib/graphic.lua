8f0052a152d844d7c41709fd5ed20989999dfa8a
asdflj <33802162+asdflj@users.noreply.github.com>
2018-03-25 15:51:53 +0800

    Add files via upload

---
+++ furnace/lib/graphic.lua
@@ -0,0 +1,299 @@
+--版本 alpha 0.2
+local component = require('component')
+local computer =require('computer')
+local event = require('event')
+local serialization =require('serialization')
+gpu = component.gpu
+ui={}
+local function text_alignment(x_min,x_max,text_len,alignment)
+	x={x_min,x_max}
+	if alignment=='right' then 
+		x.x_min=x_max-text_len+1
+		x.x_max=x_max
+	elseif alignment=='left' then 
+		x.x_min=x_min
+		x.x_max=x_max
+	elseif alignment == 'center' then
+		x.x_min=math.ceil((x_max-text_len)/2)
+		x.x_max=x_max
+	else
+		x.x_min=x_min
+		x.x_max=x_max
+	end
+	return x
+end
+local function refresh_screen()
+	ui.display_refresh()
+end
+local function write_tab(screen)
+	tab=serialization.serialize(show_display)
+	file = io.open(screen..'.cfg',"w")
+	file:write(tab)
+	file:close()
+end 
+local function read_tab(screen)
+	file = io.open(screen..'.cfg',"w")
+	tab=file:read('*a')
+	show_display=serialization.unserialize(tab)
+end
+local function text_split(text)
+	local unicode = require('unicode')
+	txt={text={},wide={},tlen=0}
+	while true do
+		text_len=string.len(text)
+		if text_len~=0 then
+			if unicode.isWide(text) then
+				t=string.sub(text,1,3)
+				text=string.sub(text,4,text_len)
+				table.insert(txt['wide'],2)
+				txt['tlen']=txt['tlen']+2
+			else
+				t=string.sub(text,1,1)
+				text=string.sub(text,2,text_len)
+				table.insert(txt['wide'],1)
+				txt['tlen']=txt['tlen']+1
+			end
+		table.insert(txt['text'],t)
+		else
+			return txt
+		end
+	end
+end
+local function draw_ui()
+	if display_pause ~= 1 then
+		ui.display_refresh()
+	end
+end
+local function mouse_click(__,__,x,y)
+	if show_display[x][y].run~=nil then
+		show_display[x][y].run()
+	end
+end
+local function mouse_scroll(__,__,__,__,r)
+	if r==1 then 
+		if show_display[show_display_area['x_min']][show_display_area['y_min']-1]~=nil then
+		show_display_area['y_min']=show_display_area['y_min']-1
+		show_display_area['y_max']=show_display_area['y_max']-1
+		show_display_area['scroll']=show_display_area['scroll']+1
+		end
+	elseif r==-1 and show_display[show_display_area['x_max']][show_display_area['y_max']+1] ~=nil then 
+		show_display_area['y_min']=show_display_area['y_min']+1
+		show_display_area['y_max']=show_display_area['y_max']+1
+		show_display_area['scroll']=show_display_area['scroll']-1
+	end
+end
+function ui.init(x,y,r)
+	show_display={}
+	display_pause=1
+	show_display_area={
+	['x_min']=1,['x_max']=x,['y_min']=1,['y_max']=y,['scroll']=0
+	}
+	if r==nil or tonumber(r)==nil then
+		show_display_refresh=3
+	else
+		show_display_refresh=r
+	end
+	for i=1,x do
+		table.insert(show_display,{})
+		for ii=1,y do
+			table.insert(show_display[i],{
+				background_colour=0x000000,
+				foreground_colour=0xFFFFFF,
+				text=' ',
+				run=nil
+			})		
+		end
+	end
+	if show_display_statu==nil then
+		show_display_statu=event.timer(show_display_refresh,draw_ui,math.huge)
+		event.listen('touch',mouse_click)
+		event.listen('gpu_bound',refresh_screen)
+		event.listen('scroll',mouse_scroll)
+	end
+end
+function ui.uninit()
+	if show_display_statu~= nil then
+		event.cancel(show_display_statu)
+		event.ignore('touch',mouse_click)
+		event.ignore('scroll',mouse_scroll)
+		event.ignore('gpu_bound',refresh_screen)
+		show_display={}
+		display_pause=0
+	end
+end
+function ui.drawline(x_min,x_max,y,background_colour,foreground_colour,text,run)
+	for x=x_min,x_max do
+		show_display[x][y]['background_colour']=background_colour
+		show_display[x][y]['foreground_colour']=foreground_colour
+		if text~=nil then 
+			show_display[x][y]['text']=text
+		end
+		if run~= nil then
+			show_display[x][y]['run']=run
+		end
+	end
+end
+function ui.fill_background_colour(background_colour)
+	for k,v in pairs(show_display) do
+		for k1,v1 in pairs(show_display[k])do
+			v1['background_colour']=background_colour
+		end
+	end
+end
+function ui.fill_foreground_colour(background_colour)
+	for k,v in pairs(show_display) do
+		for k1,v1 in pairs(show_display[k])do
+			v1['foreground_colour']=foreground_colour
+		end
+	end
+end
+function ui.clear_text()
+	for k,v in pairs(show_display) do
+		for k1,v1 in pairs(show_display[k])do
+			v1['text']=' '
+		end
+	end
+end
+function ui.setresolution(x,y)
+	gpu.setResolution(x,y)
+	show_display_area['x_min']=1
+	show_display_area['x_max']=x
+	show_display_area['y_min']=1
+	show_display_area['y_max']=y
+end
+function ui.draw_simple(x,y,background_colour,foreground_colour,text,run)
+	show_display[x][y]['background_colour']=background_colour
+	show_display[x][y]['foreground_colour']=foreground_colour
+	show_display[x][y]['text']=text
+	show_display[x][y]['run']=run
+end
+function ui.draw_text(x_min,x_max,y,text,alignment,run)
+	txt=text_split(text)
+	s=1
+	jump=0
+	for x=x_min,x_max do
+		if show_display[x]~=nil then
+			show_display[x][y]['text']=' '
+		end
+	end
+	x1=text_alignment(x_min,x_max,txt['tlen'],alignment)
+	for x=x['x_min'],x['x_max'] do
+		if jump==0 then
+			if txt['text'][s]~=nil and show_display[x]~=nil then
+				if run~=nil then
+				show_display[x][y]['run']=run
+				end
+				show_display[x][y]['text']=txt['text'][s]
+				if txt['wide'][s]==2 then
+					jump=1
+				else
+					s=s+1
+				end
+			end
+		else
+			if show_display[x]~=nil then
+				show_display[x][y]['text']=' '
+				show_display[x][y]['background_colour']=show_display[x-1][y]['background_colour']
+				show_display[x][y]['foreground_colour']=show_display[x-1][y]['foreground_colour']
+				show_display[x][y]['run']=show_display[x-1][y]['run']
+			end
+			jump=0
+			s=s+1
+		end
+	end
+end
+function ui.change_display_tab(file1,file2)
+	event.cancel(show_display_statu)
+	write_tab(file1)
+	read_tab(file2)
+	show_display_statu=event.timer(show_display_refresh,draw_ui,math.huge)
+end
+function ui.display_tab_save(file)
+	write_tab(file)
+end
+function ui.display_tab_read(file)
+	event.cancel(show_display_statu)
+	show_display={}
+	read_tab(file)
+	show_display_statu=event.timer(show_display_refresh,draw_ui,math.huge)
+end
+function ui.draw_pause()
+	if display_pause == 0 then
+		display_pause=1
+		return true
+	else
+		display_pause=0
+		return false
+	end
+end
+function ui.draw_pause_statu()
+	if display_pause == 0 then
+		return 'runing'
+	else
+		return 'dead'
+	end
+end
+function ui.change_refresh(refresh)
+	refresh=tonumber(refresh)
+	if refresh~=nil then
+		show_display_refresh=refresh
+		event.cancel(show_display_statu)
+		show_display_statu=event.timer(show_display_refresh,draw_ui,math.huge)
+		return true
+	else
+		return false
+	end
+end
+function ui.setdisplay_area(x_min,x_max,y_min,y_max)
+	local x_min=tonumber(x_min)
+	local y_min=tonumber(y_min)
+	local x_max=tonumber(x_max)
+	local y_max=tonumber(y_max)
+	if x_min ~= nil and x_max ~=nil and y_min~=nil and y_max ~=nil  then
+		show_display_area['x_min']=x_min
+		show_display_area['x_max']=x_max
+		show_display_area['y_min']=y_min
+		show_display_area['y_max']=y_max
+		return true
+	else
+		return false
+	end
+end
+function ui.getdisplay_statu()
+	if #show_display[1]~=nil then
+		local s='当前画布大小'..#show_display..'X'..#show_display[1]..',显示区域'..
+		show_display_area['x_max']..'X'..show_display_area['y_max']..'开始位置于显示区域的X:'..show_display_area['x_min']..
+		' Y:'..show_display_area['y_min']
+		return s
+	else
+		return false
+	end
+end
+function ui.display_refresh()
+	local now_foreground=gpu.getForeground()
+	local now_background=gpu.getBackground()
+	local x=1
+	for x_min=show_display_area['x_min'],show_display_area['x_max'] do
+		local y=1
+		for y_min=show_display_area['y_min'],show_display_area['y_max'] do
+			if show_display[x_min][y_min]~=nil then
+				gpu.setForeground(show_display[x_min][y_min]['foreground_colour'])
+				gpu.setBackground(show_display[x_min][y_min]['background_colour'])
+				gpu.set(x,y,show_display[x_min][y_min]['text'])
+			end
+		y=y+1
+		end
+	x=x+1
+	end
+	gpu.setForeground(now_foreground)
+	gpu.setBackground(now_background)
+end
+function ui.clear_text_line(y)
+	local y=tonumber(y)
+	if  y ~=nil and show_display[1] then
+		for x=1,#show_display[1] do
+			show_display[x][y]['text']=' '
+		end
+	end
+end
+return ui
---
+++ furnace/main.lua
@@ -0,0 +1,76 @@
+--熔炉监控器 版本 alpha 0.1
+local component = require('component')
+local graphic=require('graphic')
+local colours =require('colours')
+local thread = require("thread")
+local computer = require('computer')
+local event = require('event')
+local proxy=component.proxy
+function print_author()
+	graphic.draw_text(1,50,16,'asdflj','center')
+	graphic.display_refresh()
+	os.sleep(3)
+	graphic.clear_text_line(16)
+end
+local function find_furnace()
+	furnace={}
+	for k,v in pairs(component.list('furnace')) do
+		table.insert(furnace,k)
+	end
+	return furnace
+end
+function furnace_monitor()
+	while true do
+		statu,address,item_type=event.pull('component')
+		if statu=='component_removed' then
+			for k,v in pairs(furnace) do
+				if v==address then
+					graphic.drawline(1,50,#furnace+2,colours.black,colours.white,' ')
+					table.remove(furnace,k)
+				end
+			end
+		elseif statu=='component_added' and item_type == 'furnace' then
+			table.insert(furnace,address)
+		end
+	end
+end
+function listen()
+	if _G.thread == nil then
+		_G.thread=thread.create(furnace_monitor)
+	elseif _G.thread:status() == 'dead' then
+		_G.thread=thread.create(furnace_monitor)
+	end
+end
+function display()
+		k1=1
+	for k,v in pairs(furnace) do
+		a=proxy(v)
+		if a~= nil then
+			a_cooktime=a.getCookTime()
+			graphic.drawline(8,36,k1+2,colours.black,colours.white,' ')
+			if math.floor(a_cooktime/8)~=0 then
+				graphic.drawline(8,8+math.floor(a_cooktime/8),k1+2,colours.red,colours.white,' ')
+			end
+			graphic.draw_text(2,5,k1+2,string.sub(v,1,4),'left')
+			graphic.draw_text(37,42,k1+2,math.modf(a_cooktime/200*100)..' %','left')
+			graphic.draw_text(45,50,k1+2,tostring(a.isBurning()),'left')
+			k1=k1+1
+		end
+	end
+end
+graphic.init(50,32,5) 
+graphic.setresolution(50,16)
+local x=50
+local y=16
+graphic.draw_text(1,50,1,'furnace','center',print_author)
+graphic.draw_text(1,50,2,' name          progress         Percentage  burn','left')
+furnace=find_furnace()
+event.listen('listen',listen)
+computer.pushSignal('listen')
+--graphic.draw_pause()
+while true do
+display()
+graphic.display_refresh()
+os.sleep(1)
+end
+
